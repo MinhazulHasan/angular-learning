88b3e889da33411a8c7ddefe411baf2057faa9f2
New Account Name <minhazul.hasan.sohan@gmail.com>
2023-07-09 03:58:21 +0600

    learning directives

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -4,14 +4,27 @@ import { Component } from '@angular/core';
   selector: 'app-server',
   templateUrl: './server.component.html',
   styles: [`
-
+    .online {
+      color: white;
+    }
+    .offline {
+      color: yellow;
+    }
   `]
 })
 export class ServerComponent {
   serverId: number = 10;
   serverStatus: string = 'offline';
 
+  constructor() {
+    this.serverStatus = Math.random() >= 0.5 ? 'online' : 'offline';
+  }
+
   getServerStatus() {
     return this.serverStatus;
+  }
+
+  getBgColor() {
+    return this.serverStatus === 'online' ? 'mediumspringgreen' : 'hotPink';
   }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,6 +11,8 @@ export class ServersComponent {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'Testserver';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
 
   constructor() {
     setTimeout(() => {
@@ -22,6 +24,8 @@ export class ServersComponent {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created!';
   }
 
