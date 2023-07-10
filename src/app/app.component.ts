e7cf68817c2d2188348a1c721c90e919b5db5dc4
New Account Name <minhazul.hasan.sohan@gmail.com>
2023-07-11 02:52:01 +0600

    assignment for event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,4 +12,17 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
+
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+
+  constructor() {}
+
+  onIntervalFired(firedNumber: number) {
+    if (firedNumber % 2 === 0) {
+      this.evenNumbers.push(firedNumber);
+    } else {
+      this.oddNumbers.push(firedNumber);
+    }
+  }
 }
