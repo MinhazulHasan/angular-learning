82b35a358d08b7a824755cc3975a435d2ea8a302
New Account Name <minhazul.hasan.sohan@gmail.com>
2023-07-26 18:53:23 +0600

    service and dependency injection learning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from './services/account.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
-  styles: [`
-    h3 {
-      color: dodgerblue;
-    }
-  `]
+  styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
+  title: string = 'my-first-app';
+  accounts: { name: string, status: string }[] = [];
+  constructor(private accountService: AccountService) { }
+
+  ngOnInit() {
+    this.accounts = this.accountService.accounts;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AccountComponent } from './account/account.component';
+import { NewAccountComponent } from './new-account/new-account.component';
+import { FormsModule } from '@angular/forms';
+import { AccountService } from './services/account.service';
+import { LoggingService } from './services/logging.service';
 
 @NgModule({
   declarations: [
     AppComponent,
+    AccountComponent,
+    NewAccountComponent,
   ],
   imports: [
     BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    AccountService,
+    LoggingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
